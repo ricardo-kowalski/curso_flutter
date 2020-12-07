218ea886e9ebba508a8b5a20db223e300be3559f
Ricardo <kowalski.kgb@gmail.com>
2020-12-07 18:17:25 -0300

    provider

--- shop/lib/providers/counter_provider.dart
+++ shop/lib/providers/counter_provider.dart
@@ -5,7 +5,7 @@ class CounterState {
 
   void inc() => _value++;
   void dec() => _value--;
-  void get value => _value;
+  int get value => _value;
 
   bool diff(CounterState old) {
     return old == null || old._value != _value;
@@ -16,6 +16,10 @@ class CounterProvider extends InheritedWidget {
   final CounterState state = CounterState();
 
   CounterProvider({Widget child}) : super(child: child);
+
+  static CounterProvider of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<CounterProvider>();
+  }
 
   @override
   bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
